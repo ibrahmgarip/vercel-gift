41ea2e3b1d3aae20b1abf94fa884cee856bc6070
İbrahim <ibrahmgarip@gmail.com>
2025-06-09 23:07:38 +0300

    fix the width of the slideshow component

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Slideshow = () => {
   return (
-    <div className="w-full h-64 bg-gray-200 flex items-center justify-center">
+    <div className="w-full max-w-7xl h-64 bg-gray-200 flex items-center justify-center mx-auto">
       <p className="text-gray-600 text-xl">Slideshow Placeholder</p>
     </div>
   );
